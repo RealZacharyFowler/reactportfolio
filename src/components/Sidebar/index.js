7dba512e27c4f6cf7b18857e5b49622e55f2d664
Zac  Fowler <zfowler@nocitobenefit.com>
2023-08-22 09:59:28 -0700

    navbar completed

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -4,6 +4,11 @@ import LogoS from '../../assets/images/logo-s.png'
 import LogoSubtitle from '../../assets/images/logo_sub.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEnvelope, faHome, faUser} from '@fortawesome/free-solid-svg-icons'
+import {
+    faLinkedin,
+    faGithub,
+    faInstagram,
+} from '@fortawesome/free-brands-svg-icons'
 
 const Sidebar = () => (
     <div className='nav-bar'>
@@ -22,6 +27,36 @@ const Sidebar = () => (
                 <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
             </NavLink>
         </nav>
+        <ul>
+            <li>
+                <a 
+                target='_blank' 
+                rel='noreferrer'
+                href='https://www.linkedin.com/in/fowlerzachary/'
+                >
+                <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+                </a>
+            </li>
+            <li>
+                <a 
+                target='_blank' 
+                rel='noreferrer'
+                href='https://github.com/RealZacharyFowler'
+                >
+                <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+                </a>
+            </li>
+            <li>
+                <a 
+                target='_blank' 
+                rel='noreferrer'
+                href='https://www.instagram.com/realzacfowler/'
+                >
+                <FontAwesomeIcon icon={faInstagram} color="#4d4d4e" />
+                </a>
+            </li>
+            
+        </ul>
     </div>
 )
 
